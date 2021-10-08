234e64c2ff79f7d8a214ca11d17d0d117254df2c
Adrien Allard <adrien.allard.pro@gmail.com>
2021-10-09 00:55:34 +0200

    Change redirect Uri depending on env

--- src/app/authentication/authentication.component.ts
+++ src/app/authentication/authentication.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { SpotifyApiService } from '../spotify-api.service';
 
 @Component({
@@ -20,12 +21,9 @@ export class AuthenticationComponent implements OnInit {
         
         // https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow-with-proof-key-for-code-exchange-pkce
         
-        const redirectUri: string = 'http://localhost:4200/callback/';
-        // const redirectUri: string = 'http://browsify.aallard.net/callback/';
-        
         let state = this.router.url;
         if (state.startsWith("/auth")) {
-            this.spotifyApi.askForCode(redirectUri);
+            this.spotifyApi.askForCode(environment.redirectUri);
         } else if (state.startsWith("/callback")) {
             this.route.queryParamMap.subscribe(paramMap => {
                 let state = paramMap.get('state');
@@ -45,7 +43,7 @@ export class AuthenticationComponent implements OnInit {
                 let code = paramMap.get('code');
                 if (code !== null) {
                     try {
-                        this.spotifyApi.refreshTokenFromCode(redirectUri, code)
+                        this.spotifyApi.refreshTokenFromCode(environment.redirectUri, code)
                             .then(() => this.router.navigateByUrl('/'));
                     } catch(error) {
                         this.status = error as string;
