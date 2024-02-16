62abf20d969b3a78490d8a50375758f0c7d44c82
Adrien Allard <adrien.allard.pro@gmail.com>
2024-02-16 13:11:48 +0100

    fix: issue where redirect uri was not using https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import packageInfo from '../../package.json';
 export const environment = {
   production: true,
   spotifyClientId: '1091f9db9b7d4f51b47f57b3a766c0dc',
-  redirectUri: 'http://browsify.aallard.net/callback/',
+  redirectUri: 'https://browsify.aallard.net/callback/',
   version: packageInfo.version,
   albumCount: 12,
 };
