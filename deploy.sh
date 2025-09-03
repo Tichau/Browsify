506fc79b0bf4d6fe9fce2f88692907942f94d2a2
Adrien Allard <adrien.allard.pro@gmail.com>
2025-09-03 15:26:21 +0200

    chore: format bash file

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,24 @@
-if (( $# != 1 )); then
-    >&2 echo "usage: deploy <deploy-branch>"
-    exit
+if (($# != 1)); then
+  >&2 echo "usage: deploy <deploy-branch>"
+  exit
 fi
 
 deployBranch=$1
 
 echo "Increment build number..."
 
-json=`cat package.json`
+json=$(cat package.json)
 
 re="\"(version)\": \"([^\"]*)\""
 if [[ $json =~ $re ]]; then
-    version="${BASH_REMATCH[2]}"
+  version="${BASH_REMATCH[2]}"
 fi
 
 re="([0-9]+)\.([0-9]+)\.([0-9]+)"
 if [[ $version =~ $re ]]; then
-    major="${BASH_REMATCH[1]}"
-    minor="${BASH_REMATCH[2]}"
-    build="${BASH_REMATCH[3]}"
+  major="${BASH_REMATCH[1]}"
+  minor="${BASH_REMATCH[2]}"
+  build="${BASH_REMATCH[3]}"
 fi
 
 build=$(($build + 1))
@@ -26,17 +26,16 @@ newVersion="$major.$minor.$build"
 
 json=${json/\"version\": \"$version\"/\"version\": \"$newVersion\"}
 
-echo "$json" > package.json
+echo "$json" >package.json
 
 echo ""
 echo "Build and deploy version $newVersion..."
 
 ng deploy --dir=dist/browsify/ --cname=browsify.aallard.net --branch=$deployBranch --message="Deploy v$newVersion"
 
-if [ $? != 0 ]
-then
-    >&2 echo "Build failed."
-    exit 1
+if [ $? != 0 ]; then
+  >&2 echo "Build failed."
+  exit 1
 fi
 
 echo ""
